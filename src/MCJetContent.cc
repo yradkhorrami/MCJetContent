8793567b2ac09d042b3314149429179545339cc6
Yasser Radkhorrami <yasser.radkhorrami@desy.de>
2020-07-16 17:40:27 +0200

    Create MCJetContent.cc

---
+++ src/MCJetContent.cc
@@ -0,0 +1,14 @@
+#include "MCJetContent.h"
+#include <iostream>
+#include <vector>
+#include <string>
+#include <EVENT/LCCollection.h>
+#include <EVENT/MCParticle.h>
+#include <EVENT/ReconstructedParticle.h>
+#include "IMPL/ReconstructedParticleImpl.h"
+#include "IMPL/LCCollectionVec.h"
+#include <EVENT/LCCollection.h>
+
+using namespace lcio ;
+using namespace marlin ;
+using namespace std ;
